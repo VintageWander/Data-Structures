52d9595af8f1fddd3c570ee5ce14c9ff5ffb2290
my life kinda sucks <nguyendtgiang@gmail.com>
2021-03-07 23:32:17 +0700

    refactor

--- Algorithms/nQueens/nQueens.cpp
+++ Algorithms/nQueens/nQueens.cpp
@@ -1,37 +1,30 @@
 #include <iostream>
 #include <vector>
 #include <string>
+using std::vector, std::string;
 
-bool isValid(int row, int col, int size, std::vector<std::vector<std::string>> &board)
+bool isValid(int row, int col, int size, vector<vector<string>> &board)
 {
-    for (int a = 1; row - a >= 0; a++) 
-    {
+    for (int a = 1; row - a >= 0; a++) {
         if (board[row - a][col] == "Q"){ return false; }
     }
-    for (int a = 1, b = 1; row - a >= 0 && col - b >= 0; a++ && b++) 
-    {
+    for (int a = 1, b = 1; row - a >= 0 && col - b >= 0; a++ && b++) {
         if (board[row - a][col - b] == "Q") { return false;}
     }
-    for (int a = 1, b = 1; row - a >= 0 && col + b < size; a++ && b++) 
-    {
+    for (int a = 1, b = 1; row - a >= 0 && col + b < size; a++ && b++) {
         if (board[row - a][col + b] == "Q"){ return false; }
     }
     return true;
 };
 
-void Solve(int row, int col, int size, std::vector<std::vector<std::string>> &board, std::vector<std::vector<std::vector<std::string>>> &solutions)
-{
-    if (row == size)
-    {
-        if (board[row - 1][col] == "Q")
-        {
+void Solve(int row, int col, int size, vector<vector<string>> &board, vector<vector<vector<string>>> &solutions){
+    if (row == size){
+        if (board[row - 1][col] == "Q"){
             solutions.emplace_back(board);
         }
     }
-    for (int i = 0; i < size; ++i)
-    {
-        if (isValid(row, i, size, board))
-        {
+    for (int i = 0; i < size; ++i){
+        if (isValid(row, i, size, board)){
             board[row][i] = "Q";
             Solve(row + 1, i, size, board, solutions);
             board[row][i] = ".";
@@ -39,15 +32,12 @@ void Solve(int row, int col, int size, std::vector<std::vector<std::string>> &bo
     }
 };
 
-std::vector<std::vector<std::vector<std::string>>> SolveNQueens(int n)
-{
-    std::vector<std::vector<std::vector<std::string>>> solutions;
-    std::vector<std::vector<std::string>> board;
-    for (int i = 0; i < n; i++)
-    {
-        std::vector<std::string> lines;
-        for (int j = 0; j < n; j++)
-        {
+vector<vector<vector<string>>> SolveNQueens(int n){
+    vector<vector<vector<string>>> solutions;
+    vector<vector<string>> board;
+    for (int i = 0; i < n; i++){
+        vector<string> lines;
+        for (int j = 0; j < n; j++){
             lines.emplace_back(".");
         }
         board.emplace_back(lines);
@@ -61,25 +51,20 @@ int main()
 {
     int n = 8;
     auto test = SolveNQueens(n);
-    for (int i = 0; i < test.size(); i++)
-    {
-        for (int g = 0; g < n*2 + 2; g++)
-        {
+    for (int i = 0; i < test.size(); i++){
+        for (int g = 0; g < n*2 + 2; g++){
             std::cout << "_";
         }
         std::cout << "\n";
-        for (int j = 0; j < n; j++)
-        {
+        for (int j = 0; j < n; j++){
             std::cout << "|";
-            for (int m = 0; m < n; m++)
-            {
+            for (int m = 0; m < n; m++){
                 std::cout << test[i][j][m] << " ";
             }
             std::cout << "|";
             std::cout << "\n";
         }
-        for (int g = 0; g < n*2 + 2; g++)
-        {
+        for (int g = 0; g < n*2 + 2; g++){
             std::cout << "-";
         }
         std::cout << "\n";

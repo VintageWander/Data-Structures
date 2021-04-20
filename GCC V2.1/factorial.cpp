ce74f176532e8326b7fb500572c69a91dcc68bb8
my life kinda sucks <nguyendtgiang@gmail.com>
2021-04-20 20:29:29 +0700

    Update factorial.cpp

--- GCC V2.1/factorial.cpp
+++ GCC V2.1/factorial.cpp
@@ -5,21 +5,21 @@
 
 std::string factorial(int n){
 	int storage[200] = {1}, size = 1; 
-    std::string result;
+	std::string result;
 	for (int i = 2; i <= n; i++) {
-        int temp = 0;
-        for (int j = 0; j < size; j++) {
-            int product = storage[j] * i + temp;
-            storage[j] = product % 10;
-            temp = product / 10;
-        }
-        while (temp != 0){
-            storage[size++] = temp % 10;
-            temp /= 10;
-        }
-    }
+    	int temp = 0;
+    	for (int j = 0; j < size; j++) {
+			int product = storage[j] * i + temp;
+			storage[j] = product % 10;
+			temp = product / 10;
+		}
+		while (temp != 0){
+			storage[size++] = temp % 10;
+			temp /= 10;
+		}
+	}
 	for(int i = size - 1; i >= 0; i--) result += std::to_string(storage[i]);
-  	return result;
+	return result;
 }
 
 int main() {

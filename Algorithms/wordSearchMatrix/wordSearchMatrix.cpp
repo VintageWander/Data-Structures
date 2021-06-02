f8b10afb3a753eafc51e29a88e4fce17420a1730
my life kinda sucks <nguyendtgiang@gmail.com>
2021-06-02 11:23:58 +0700

    update algorithm

--- Algorithms/wordSearchMatrix/wordSearchMatrix.cpp
+++ Algorithms/wordSearchMatrix/wordSearchMatrix.cpp
@@ -10,27 +10,27 @@ public:
         if (word.empty()) return true;
         if (board.empty() || board[0].empty()) return false;
         
-        m = board.size();
-        n = board[0].size();
-        
-        for (int i = 0; i < m; ++i)
-            for (int j = 0; j < n; ++j)
-                if (dfsSearch(board, word, 0, i, j)) return true;
+        for (int i = 0; i < board.size(); ++i)
+            for (int j = 0; j < board[0].size(); ++j)
+                if (dfs(board, word, 0, i, j)) return true;
         return false;
     }
 private:
-    int m;
-    int n;
-    bool dfsSearch(vector<vector<char>>& board, string& word, int k, int i, int j) {
-        if (i < 0 || i >= m || j < 0 || j >= n || word[k] != board[i][j]) return false;
+    bool dfs(vector<vector<char>>& board, string& word, int k, int i, int j) {
+        if (i < 0 
+         || i >= board.size() 
+         || j < 0 
+         || j >= board[0].size() 
+         || word[k] != board[i][j]) return false;
+
         if (k == word.length() - 1) return true; 
 
         char cur = board[i][j];
         board[i][j] = '*'; 
-        bool search_next = dfsSearch(board, word, k+1, i-1 ,j) 
-                        || dfsSearch(board, word, k+1, i+1, j) 
-                        || dfsSearch(board, word, k+1, i, j-1)
-                        || dfsSearch(board, word, k+1, i, j+1);
+        bool search_next = dfs(board, word, k+1, i-1 ,j) 
+                        || dfs(board, word, k+1, i+1, j) 
+                        || dfs(board, word, k+1, i, j-1)
+                        || dfs(board, word, k+1, i, j+1);
         board[i][j] = cur; 
         return search_next;
     }
